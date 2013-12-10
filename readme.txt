40930cd5e0a4ad854278b51eb2984cd172cfa14c
ethitter <ethitter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-10 16:05:30 +0000

    WP Revisions Control: release v1.2.1, featuring post-level revision purging and a Spanish translation
    Version 1.2 (unreleased) introduces post-level control over the number of revisions stored for a given post. One can now purge all revisions for a specific post as well.
    
    Thanks to Maria Ramos at Web Hosting Hub, the plugin is now translated into Spanish as well.
    
    See https://github.com/ethitter/WP-Revisions-Control for a complete changelog.
    
    
    git-svn-id: https://plugins.svn.wordpress.org/wp-revisions-control/tags/1.2.1@818321 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: ethitter
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=10275434
 Tags: revision, revisions, admin
 Requires at least: 3.6
-Tested up to: 3.6
-Stable tag: 1.0
+Tested up to: 3.8
+Stable tag: 1.2.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -33,9 +33,24 @@ Navigate to **Settings > Writing** in your WordPress Dashboard, and look for the
 
 == Changelog ==
 
+= 1.2.1 =
+* Introduce Spanish translation thanks to Maria Ramos at [WebHostingHub](http://www.webhostinghub.com/).
+
+= 1.2 =
+* Add post-level revision purging and limiting. For any post type that supports revisions, you can now limit the number of revisions retained at a post level.
+
 = 1.0 =
-Initial public release
+* Initial public release
+
+== Upgrade Notice ==
+
+= 1.2.1 =
+Introduces Spanish translation thanks to Maria Ramos at [WebHostingHub](http://www.webhostinghub.com/).
+
+= 1.2 =
+For any post type that supports revisions, you can now limit the number of revisions retained at a post level.
 
 == Screenshots ==
 
 1. The plugin's settings section, found under **Settings > Writing**.
+2. The post-level controls provided in version 1.2.
--- wp-revisions-control.php
+++ wp-revisions-control.php
@@ -4,8 +4,10 @@ Plugin Name: WP Revisions Control
 Plugin URI: http://www.ethitter.com/plugins/wp-revisions-control/
 Description: Control how many revisions are stored for each post type
 Author: Erick Hitter
-Version: 1.0
+Version: 1.2.1
 Author URI: http://www.ethitter.com/
+Text Domain: wp_revisions_control
+Domain Path: /languages/
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -40,6 +42,8 @@ class WP_Revisions_Control {
 	private $settings_page = 'writing';
 	private $settings_section = 'wp_revisions_control';
 
+	private $meta_key_limit = '_wp_rev_ctl_limit';
+
 	/**
 	 * Silence is golden!
 	 */
@@ -68,7 +72,20 @@ class WP_Revisions_Control {
 	 * @return null
 	 */
 	private function setup() {
+		add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
 		add_action( 'init', array( $this, 'action_init' ) );
+	}
+
+	/**
+	 * Load plugin translations
+	 *
+	 * @uses load_plugin_textdomain
+	 * @uses plugin_basename
+	 * @action plugins_loaded
+	 * @return null
+	 */
+	public function action_plugins_loaded() {
+		load_plugin_textdomain( 'wp_revisions_control', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
@@ -86,7 +103,7 @@ class WP_Revisions_Control {
 	}
 
 	/**
-	 * Register plugin's settings fields
+	 * Register plugin's admin-specific elements
 	 *
 	 * Plugin title is intentionally not translatable.
 	 *
@@ -99,6 +116,7 @@ class WP_Revisions_Control {
 	 * @return null
 	 */
 	public function action_admin_init() {
+		// Plugin setting section
 		register_setting( $this->settings_page, $this->settings_section, array( $this, 'sanitize_options' ) );
 
 		add_settings_section( $this->settings_section, 'WP Revisions Control', array( $this, 'settings_section_intro' ), $this->settings_page );
@@ -106,7 +124,17 @@ class WP_Revisions_Control {
 		foreach ( $this->get_post_types() as $post_type => $name ) {
 			add_settings_field( $this->settings_section . '-' . $post_type, $name, array( $this, 'field_post_type' ), $this->settings_page, $this->settings_section, array( 'post_type' => $post_type ) );
 		}
+
+		// Post-level functionality
+		add_action( 'add_meta_boxes', array( $this, 'action_add_meta_boxes' ), 10, 2 );
+		add_action( 'wp_ajax_' . $this->settings_section . '_purge', array( $this, 'ajax_purge' ) );
+		add_action( 'save_post', array( $this, 'action_save_post' ) );
 	}
+
+	/**
+	 ** PLUGIN SETTINGS SECTION
+	 ** FOUND UNDER SETTINGS > WRITING
+	 **/
 
 	/**
 	 * Display assistive text in settings section
@@ -124,7 +152,7 @@ class WP_Revisions_Control {
 		// Display a note if the plugin priority is other than the default.
 		// Will be useful when debugging issues later.
 		if ( $this->plugin_priority() !== $this->priority_default ) : ?>
-			<p><?php _e( "A local change is causing this plugin's functionality to run at a priority other than the default. If you experience difficulties with the plugin, please unhook any functions from the <code>wp_revisions_control_priority</code> filter.", 'wp_revisions_control' ); ?></p>
+			<p><?php printf( __( "A local change is causing this plugin's functionality to run at a priority other than the default. If you experience difficulties with the plugin, please unhook any functions from the %s filter.", 'wp_revisions_control' ), '<code>wp_revisions_control_priority</code>' ); ?></p>
 		<?php endif;
 	}
 
@@ -171,6 +199,10 @@ class WP_Revisions_Control {
 	}
 
 	/**
+	 ** REVISIONS QUANTITY OVERRIDES
+	 **/
+
+	/**
 	 * Allow others to change the priority this plugin's functionality runs at
 	 *
 	 * @uses apply_filters
@@ -189,20 +221,195 @@ class WP_Revisions_Control {
 	/**
 	 * Override number of revisions to keep using plugin's settings
 	 *
+	 * Can either be post-specific or universal
+	 *
+	 * @uses get_post_meta
 	 * @uses get_post_type
 	 * @uses this::get_settings
 	 * @filter wp_revisions_to_keep
 	 * @return mixed
 	 */
 	public function filter_wp_revisions_to_keep( $qty, $post ) {
-		$post_type = get_post_type( $post ) ? get_post_type( $post ) : $post->post_type;
-		$settings = $this->get_settings();
+		$post_limit = get_post_meta( $post->ID, $this->meta_key_limit, true );
 
-		if ( array_key_exists( $post_type, $settings ) )
-			return $settings[ $post_type ];
+		if ( 0 < strlen( $post_limit ) ) {
+			$qty = $post_limit;
+		} else {
+			$post_type = get_post_type( $post ) ? get_post_type( $post ) : $post->post_type;
+			$settings = $this->get_settings();
+
+			if ( array_key_exists( $post_type, $settings ) )
+				$qty = $settings[ $post_type ];
+		}
 
 		return $qty;
 	}
+
+	/**
+	 ** POST-LEVEL FUNCTIONALITY
+	 **/
+
+	/**
+	 * Override Core's revisions metabox
+	 *
+	 * @param string $post_type
+	 * @param object $post
+	 * @uses post_type_supports
+	 * @uses get_post_status
+	 * @uses wp_get_post_revisions
+	 * @uses remove_meta_box
+	 * @uses add_meta_box
+	 * @uses wp_enqueue_script
+	 * @uses plugins_url
+	 * @uses wp_localize_script
+	 * @uses wpautop
+	 * @uses add_action
+	 * @action add_meta_boxes
+	 * @return null
+	 */
+	public function action_add_meta_boxes( $post_type, $post ) {
+		if ( post_type_supports( $post_type, 'revisions' ) && 'auto-draft' != get_post_status() && count( wp_get_post_revisions( $post ) ) > 1 ) {
+			// Replace the metabox
+			remove_meta_box( 'revisionsdiv', null, 'normal' );
+			add_meta_box( 'revisionsdiv-wp-rev-ctl', __('Revisions'), array( $this, 'revisions_meta_box' ), null, 'normal', 'core' );
+
+			// A bit of JS for us
+			$handle = 'wp-revisions-control-post';
+			wp_enqueue_script( $handle, plugins_url( 'js/post.js', __FILE__ ), array( 'jquery' ), '20131205', true );
+			wp_localize_script( $handle, $this->settings_section, array(
+				'namespace'       => $this->settings_section,
+				'action_base'     => $this->settings_section,
+				'processing_text' => __( 'Processing&hellip;', 'wp_revisions_control' ),
+				'ays'             => __( 'Are you sure you want to remove revisions from this post?', 'wp_revisions_control' ),
+				'autosave'        => __( 'Autosave' ),
+				'nothing_text'    => wpautop( __( 'There are no revisions to remove.', 'wp_revisions_control' ) ),
+				'error'           => __( 'An error occurred. Please refresh the page and try again.', 'wp_revisions_control' )
+			) );
+
+			// Add some styling to our metabox additions
+			add_action( 'admin_head', array( $this, 'action_admin_head' ), 999 );
+		}
+	}
+
+	/**
+	 * Render Revisions metabox with plugin's additions
+	 *
+	 * @uses post_revisions_meta_box
+	 * @uses the_ID
+	 * @uses esc_attr
+	 * @uses wp_create_nonce
+	 * @uses this::get_post_revisions_to_keep
+	 * @uses wp_nonce_field
+	 * @return string
+	 */
+	public function revisions_meta_box( $post ) {
+		post_revisions_meta_box( $post );
+
+		?>
+		<div id="<?php echo esc_attr( $this->settings_section ); ?>">
+			<h4>WP Revisions Control</h4>
+
+			<p class="button purge" data-postid="<?php the_ID(); ?>" data-nonce="<?php echo esc_attr( wp_create_nonce( $this->settings_section . '_purge' ) ); ?>"><?php _e( 'Purge these revisions', 'wp_revisions_control' ); ?></p>
+
+			<p>
+				<?php printf( __( 'Limit this post to %s revisions. Leave this field blank for default behavior.', 'wp_revisions_control' ), '<input type="text" name="' . $this->settings_section . '_qty" value="' . $this->get_post_revisions_to_keep( $post->ID ) . '" id="' . $this->settings_section . '_qty" size="2" />' ); ?>
+
+				<?php wp_nonce_field( $this->settings_section . '_limit', $this->settings_section . '_limit_nonce', false ); ?>
+			</p>
+		</div><!-- #<?php echo esc_attr( $this->settings_section ); ?> -->
+		<?php
+	}
+
+	/**
+	 * Process a post-specific request to purge revisions
+	 *
+	 * @uses __
+	 * @uses check_ajax_referer
+	 * @uses current_user_can
+	 * @uses wp_get_post_revisions
+	 * @uses number_format_i18n
+	 * @return string
+	 */
+	public function ajax_purge() {
+		$post_id = isset( $_REQUEST['post_id'] ) ? (int) $_REQUEST['post_id'] : false;
+
+		// Hold the current state of this Ajax request
+		$response = array();
+
+		// Check for necessary data and capabilities
+		if ( ! $post_id )
+			$response['error'] = __( 'No post ID was provided. Please refresh the page and try again.', 'wp_revisions_control' );
+		elseif ( ! check_ajax_referer( $this->settings_section . '_purge', 'nonce', false ) )
+			$response['error'] = __( 'Invalid request. Please refresh the page and try again.', 'wp_revisions_control' );
+		elseif ( ! current_user_can( 'edit_post', $post_id ) )
+			$response['error'] = __( 'You are not allowed to edit this post.', 'wp_revisions_control' );
+
+		// Request is valid if $response is still empty, as no errors arose above
+		if ( empty( $response ) ) {
+			$revisions = wp_get_post_revisions( $post_id );
+
+			$count = count( $revisions );
+
+			foreach ( $revisions as $revision ) {
+				wp_delete_post_revision( $revision->ID );
+			}
+
+			$response['success'] = sprintf( __( 'Removed %s revisions associated with this post.', 'wp_revisions_control' ), number_format_i18n( $count, 0 ) );
+			$response['count'] = $count;
+		}
+
+		// Pass the response back to JS
+		echo json_encode( $response );
+		exit;
+	}
+
+	/**
+	 * Sanitize and store post-specifiy revisions quantity
+	 *
+	 * @uses wp_verify_nonce
+	 * @uses update_post_meta
+	 * @action save_post
+	 * @return null
+	 */
+	public function action_save_post( $post_id ) {
+		if ( isset( $_POST[ $this->settings_section . '_limit_nonce' ] ) && wp_verify_nonce( $_POST[ $this->settings_section . '_limit_nonce' ], $this->settings_section . '_limit' ) && isset( $_POST[ $this->settings_section . '_qty' ] ) ) {
+			$limit = $_POST[ $this->settings_section . '_qty' ];
+
+			if ( -1 == $limit || empty( $limit ) )
+				delete_post_meta( $post_id, $this->meta_key_limit );
+			else
+				update_post_meta( $post_id, $this->meta_key_limit, absint( $limit ) );
+		}
+	}
+
+	/**
+	 * Add a border between the regular revisions list and this plugin's additions
+	 *
+	 * @uses esc_attr
+	 * @action admin_head
+	 * @return string
+	 */
+	public function action_admin_head() {
+	?>
+		<style type="text/css">
+			#revisionsdiv-wp-rev-ctl #<?php echo esc_attr( $this->settings_section ); ?> {
+				 border-top: 1px solid #dfdfdf;
+				 padding-top: 0;
+				 margin-top: 20px;
+			}
+
+			#revisionsdiv-wp-rev-ctl #<?php echo esc_attr( $this->settings_section ); ?> h4 {
+				border-top: 1px solid #fff;
+				padding-top: 1.33em;
+				margin-top: 0;
+			}
+		</style>
+	<?php
+	}
+
+	/**
+	 ** PLUGIN UTILITIES
+	 **/
 
 	/**
 	 * Retrieve plugin settings
@@ -278,6 +485,24 @@ class WP_Revisions_Control {
 			return '';
 		else
 			return (int) $to_keep;
+	}
+
+	/**
+	 * Retrieve number of revisions to keep for a give post
+	 *
+	 * @param int $post_id
+	 * @uses get_post_meta
+	 * @return mixed
+	 */
+	private function get_post_revisions_to_keep( $post_id ) {
+		$to_keep = get_post_meta( $post_id, $this->meta_key_limit, true );
+
+		if ( -1 == $to_keep || empty( $to_keep ) )
+			$to_keep = '';
+		else
+			$to_keep = (int) $to_keep;
+
+		return $to_keep;
 	}
 }
 WP_Revisions_Control::get_instance();
